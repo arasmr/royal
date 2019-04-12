30e979368ca34531b16269597d635c6e55608f58
Emre Aras <e.aras@eventstravel.com>
2019-04-13 01:14:59 +0200

    hey

---
+++ build/precache-manifest.9f28513c1fe63dca67e274236d1afd1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e2e947148ed141269d7fbc2635985f8",
+    "url": "/static/media/kebap.1e2e9471.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed4ceb7ba2919bf2be4d",
+    "url": "/static/js/main.ed4ceb7b.chunk.js"
+  },
+  {
+    "revision": "a714b8381948669cfd19",
+    "url": "/static/js/1.a714b838.chunk.js"
+  },
+  {
+    "revision": "ed4ceb7ba2919bf2be4d",
+    "url": "/static/css/main.91587424.chunk.css"
+  },
+  {
+    "revision": "a714b8381948669cfd19",
+    "url": "/static/css/1.515c2064.chunk.css"
+  },
+  {
+    "revision": "ba8b0cb5093797875259decc478fa547",
+    "url": "/index.html"
+  }
+];
