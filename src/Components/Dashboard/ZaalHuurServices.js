84782e6654a3947adfaf6491435b992edcf7bfdb
Emre Aras <e.aras@eventstravel.com>
2019-04-18 01:40:48 +0200

    error fixed dasbhboard

--- src/Components/Dashboard/ZaalHuurServices.js
+++ src/Components/Dashboard/ZaalHuurServices.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,CardTitle, CardSubtitle, Col,Button } from 'reactstrap';
-import kebap from '../../assets/kebap.jpeg';
 
 const Buffet = (props) => {
   return (
     <Col sm="10">
         <Card>
-            <CardImg top style={{width:100+'%'}} src={kebap} alt="Kebap-Karisik" />
+            <CardImg top style={{width:100+'%'}} src="https://firebasestorage.googleapis.com/v0/b/newproject-6d1f4.appspot.com/o/royal%2Fpirinc-haslama.jpeg?alt=media&token=d9d152e5-ec3f-49ba-991f-d639068f1307" alt="Kebap-Karisik" />
             <CardBody>
             <CardTitle>ZALLHUUR SERVICES</CardTitle>
             <CardSubtitle>Zaalhuur Services</CardSubtitle>
