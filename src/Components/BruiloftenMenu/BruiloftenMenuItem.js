d6d9de59a2093a754760d87e77c2230f8f2c8583
Emre Aras <e.aras@eventstravel.com>
2019-07-25 01:59:06 +0200

    added

--- src/Components/BruiloftenMenu/BruiloftenMenuItem.js
+++ src/Components/BruiloftenMenu/BruiloftenMenuItem.js
@@ -22,7 +22,7 @@ class BruiloftenMenuItem extends React.Component{
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
                     <p className="card-text">{desc}</p>
-                    <div className="card-button-group" style={{margin:'1%', width:'100%'}}>
+                    <div className="card-button-group" style={{padding:'3%'}}>
                         <button hidden={!this.state.logged_in} onClick={() => this.props.addToOrder(this.props.index)} className="btn btn-sm btn-outline-primary" style={{float:'left'}}>Add To Card</button>
                         <DetailsModal
                             key={this.props.index}
